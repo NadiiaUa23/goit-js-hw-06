e75137093466a31a106427b0910d14b18a98369e
NadiiaUa23 <hasiminka92@gmail.com>
2023-11-30 15:56:14 +0100

    value, це ж нове значення , то без this!

--- js/task-1.js
+++ js/task-1.js
@@ -11,7 +11,7 @@ const customer = {
       return this.discount;
     },
     setDiscount(value) {
-        this.discount = this.value;
+        this.discount = value;
     },
     getOrders() {
       return this.orders;
